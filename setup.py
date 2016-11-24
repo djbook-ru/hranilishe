329e8d1c7cd3eb7607c11b50d1b666ac137b7dcf
Rokker Ruslan <rokkerruslan@yandex.com>
2016-11-24 17:43:59 +0300

    Created main setup config file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hranilishe',
+    version='0.0.1',
+    packages=find_packages(),
+    description='всякие полезняхи при работе с хранилищами медиа данных',
+    install_requires=[
+        'Django>=1.8'
+    ]
+)
